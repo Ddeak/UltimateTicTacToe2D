c3a302043fbcfc77f844c5ce1285d4367440d347
ddeak <ddeak@goodpractice.com>
2020-06-11 12:38:31 +0100

    Fixed a bug where the game would result in a draw when all 9 tiles were clicked but a winner was found. Added the ability to select which player starts the game.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
+public class Player {
+    public Image panel;
+    public Text text;
+    public Button button;
+}
+
+[System.Serializable]
+public class PlayerColor {
+    public Color panelColor;
+    public Color textColor;
+}
+
 public class GameController : MonoBehaviour {
     public Text[] buttonList;
     public GameObject gameOverPanel;
     public Text gameOverText;
     public GameObject resetButton;
+    public Player playerX;
+    public Player playerO;
+    public PlayerColor activePlayerColor;
+    public PlayerColor inactivePlayerColor;
 
     private string playerSide;
     private WinningOptions winOpts;
@@ -16,7 +33,6 @@ public class GameController : MonoBehaviour {
     private void Awake() {
         winOpts = new WinningOptions();
         SetGameControllerReferencesOnButtons();
-        playerSide = "X";
         gameOverPanel.SetActive(false);
         gameOverText.text = "Game Over";
         moveCount = 0;
@@ -40,11 +56,19 @@ public class GameController : MonoBehaviour {
 
         gameOverPanel.SetActive(true);
         resetButton.SetActive(true);
+        SetPlayerColorsInactive();
     }
 
     void ChangeSides ()
     {
-        playerSide = (playerSide == "X") ? "O" : "X";
+        if (playerSide == "X") {
+            playerSide = "O";
+            SetPlayerColors(playerO, playerX);
+        } else {
+            playerSide = "X";
+            SetPlayerColors(playerX, playerO);
+        }
+        
     }
 
     public string GetPlayerSide() {
@@ -52,9 +76,10 @@ public class GameController : MonoBehaviour {
     }
 
     public void EndTurn(int buttonPressed) {
-        bool gameIsADraw = ++moveCount >= 9;
+        bool gameWon = CheckForWin(buttonPressed);
+        bool gameIsADraw = ++moveCount >= 9 && !gameWon;
 
-        if (CheckForWin(buttonPressed) == true || gameIsADraw) {
+        if ( gameWon || gameIsADraw) {
             GameOver(gameIsADraw);
         } else {
             ChangeSides();
@@ -91,11 +116,48 @@ public class GameController : MonoBehaviour {
     }
 
     public void RestartGame() {
-        playerSide = "X";
         moveCount = 0;
         gameOverPanel.SetActive(false);
 
-        SetBoardInteractable(true);
         resetButton.SetActive(false);
+        SetPlayerButtons(true);
+        SetPlayerColorsInactive();
+    }
+
+    void SetPlayerColors(Player newPlayer, Player oldPlayer) {
+        newPlayer.panel.color = activePlayerColor.panelColor;
+        newPlayer.text.color = activePlayerColor.textColor;
+
+        oldPlayer.panel.color = inactivePlayerColor.panelColor;
+        oldPlayer.text.color = inactivePlayerColor.textColor;
+    }
+
+    public void SetStartingSide(string startingSide) {
+        playerSide = startingSide;
+        
+        if (playerSide == "X") {
+            SetPlayerColors(playerX, playerO);
+        } else {
+            SetPlayerColors(playerO, playerX);
+        }
+
+        StartGame();
+    }
+
+    void StartGame() {
+        SetBoardInteractable(true);
+        SetPlayerButtons(false);
+    }
+
+    void SetPlayerButtons(bool toggle) {
+        playerX.button.interactable = toggle;
+        playerO.button.interactable = toggle;
+    }
+
+    void SetPlayerColorsInactive() {
+        playerX.panel.color = inactivePlayerColor.panelColor;
+        playerX.text.color = inactivePlayerColor.textColor;
+        playerO.panel.color = inactivePlayerColor.panelColor;
+        playerO.text.color = inactivePlayerColor.textColor;
     }
 }
